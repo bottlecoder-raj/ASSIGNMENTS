29899f5e08a1bb057597309b5297002855b603e7
Raj Narayan <hazrarajnarayan55@gmail.com>
2024-11-12 17:46:40 +0530

    Update Q3-palindrome.c

--- Assignment-1/Q3-palindrome.c
+++ Assignment-1/Q3-palindrome.c
@@ -1,4 +1,53 @@
-#include<stdio.h>
+// Program to check for a palindrome string 
+// using two pointers
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void isPalindrome(char *str) {
+     
+      // Index pointer to the start
+    int left = 0;
+  
+    // Index pointer to the end
+    int right = strlen(str) - 1;
+
+    // Run the loop till both pointer
+    // meet
+    while (left < right) {
+      
+        // If characters don't match,
+        // string is not palindrome
+        if (str[left] != str[right]) {
+            printf("\"%s\" is not palindrome.\n",
+               str);
+            return;
+        }
+
+        // Move both pointers towards
+        // each other
+        left++;
+        right--;
+    }
+
+    // If all characters match,
+    // string is palindrome
+    printf("\"%s\" is palindrome.\n",
+               str);
+}
+
+int main() {
+    
+      // Checking if given strings are palindrome
+      isPalindrome("madam");
+      isPalindrome("hello");
+      return 0;
+}
+
+
+
+
+/*#include<stdio.h>
 #include<string.h>
 int main(){
     char name[50];
@@ -38,4 +87,4 @@ int main(){
         else{printf("NOT PALINDROME!");}
 
     return 0;
-}
+}*/
